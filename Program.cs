32a2d5d3c551cea024693831acff898337d94120
Jan Kubisa <s27920@pjwstk.edu.pl>
2024-03-15 09:26:54 +0100

    second commit'

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+class ConsoleApp1
+{
+    public static void Main(string[] args)
+    {
+        int total = 0;
+        int count =0;
+        int[] arr = new[] { 1, 9, 2, 8, 3, 7, 4, 6, 5 };
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            total += arr[i];
+            count = i;
+        }
 
-Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.WriteLine("avg = " + total/count);
+        }
+        catch (ArithmeticException e)
+        {
+            Console.WriteLine(total/1);
+
+        }
+        
+        
+    }
+
+    
+}
